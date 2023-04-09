b06cbec6bdbb0bc7042b649e5d48f451f6675cc6
Your Name <you@example.com>
2023-04-09 07:14:50 +0200

    syntax error fixed

--- src/adder/adder.v
+++ src/adder/adder.v
@@ -1,11 +1,11 @@
 module adder #(
-parameter  = INPUT_WIDTH=5,
-parameter  = OUTPUT_WIDTH=11
+parameter  INPUT_WIDTH=5,
+parameter  OUTPUT_WIDTH=11
 )(
 input  [INPUT_WIDTH-1:0] data_in_0,
 input  [INPUT_WIDTH-1:0] data_in_1,
 output [OUTPUT_WIDTH-1:0]data_out
 );
-data_out = data_in_0 + data_in_1;
+assign data_out = data_in_0 + data_in_1;
 
 endmodule
